9130c9d5c82238d5c1b24b640fc722168649bfa2
swastiksuman@gmail.com <swastiksuman@gmail.com>
2015-09-24 23:38:45 +0530

    demo change

--- src/test/java/com/google/search/search/GmailTest.java
+++ src/test/java/com/google/search/search/GmailTest.java
@@ -1,19 +1,34 @@
 package com.google.search.search;
 
-import org.junit.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import com.gargoylesoftware.htmlunit.javascript.configuration.BrowserName;
 
 public class GmailTest {
 	
 	WebDriver driver;
-	
+	String nodeURL;
 	
 	@Test
-	public void gmailLogin() throws InterruptedException{
-		driver = new FirefoxDriver();
+	public void gmailLogin() throws InterruptedException, MalformedURLException{
+		
+		
+		DesiredCapabilities desCaps = DesiredCapabilities.firefox();
+		desCaps.setPlatform(Platform.LINUX);
+		nodeURL = "http://localhost:4444/wd/hub";
+		driver = new RemoteWebDriver(new URL(nodeURL), desCaps);
+		
+		
 		driver.get("http://www.google.com");
 		driver.manage().window().maximize();
 		driver.findElement(By.name("q")).sendKeys("Verizon");
